b44348d0ca33a749bd1661790cda1dff58a0c8f4
Смирнов Павел <cocacola76rus@outlook.com>
2019-10-21 22:18:53 +0300

    В клас калькулятор добавлены методы  Sum, Subtract, Multiply, Division

--- MoneyCalc/Calc.cs
+++ MoneyCalc/Calc.cs
@@ -6,9 +6,29 @@ namespace MoneyCalc
 {
     class Calc
     {
-        public Calc()
+        public Calc() {}
+        public double Sum(double a, double b)
         {
-           //sadasdas
+            return a + b;
+
         }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+
+        }
+        public double Division(double a, double b)
+        {
+            return a / b;
+
+        }
+
     }
 }
